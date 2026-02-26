8f96eb7af2bf68d0d6bb421d96a69bd61b82fc58
roberdeth91 <amavizca.roberto@gmail.com>
2026-02-26 14:20:00 -0700

    auto: backup 2026-02-26 14:20:00

--- python/cale.py
+++ python/cale.py
@@ -1,8 +1,8 @@
 numeros = [10, 15, 20, 25, 30]
-resultado = []
+resultado = [n for n in numeros if n > 20]
 
-for n in numeros:
-    if n > 20:
-        resultado.append(n / 2)
+#for n in numeros:
+#    if n > 20:
+#        resultado.append(n / 2)
 
 print(resultado)
