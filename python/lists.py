a88f342303ea0c1652091c4767f2674e4967e993
roberdeth91 <amavizca.roberto@gmail.com>
2026-02-28 17:10:00 -0700

    auto: backup 2026-02-28 17:10:00

--- python/lists.py
+++ python/lists.py
@@ -1,14 +1,22 @@
-N = int(input())
-arr = []
-arr.insert(0,5)
-arr.insert(1,10)
-arr.insert(0,6)
-print(arr)
-arr.remove(6)
-arr.append(9)
-arr.append(1)
-arr.sort()
-print(arr)
-arr.pop()
-arr.sort(reverse=True)
-print(arr)
+if __name__ == '__main__':
+    N = int(input())
+    arr = []
+for i in range(N):
+    partes = input().split()
+    if partes[0] == "append":
+        arr.append(int(partes [1]))
+    elif partes[0] == "print":
+        print(arr)
+    elif partes[0] == "insert":
+        arr.insert(int(partes[1]), int(partes[2]))
+    elif partes[0] == "remove":
+        arr.remove(int(partes[1]))
+    elif partes[0] == "sort":
+        arr.sort()
+    elif partes[0] == "pop":
+        arr.pop()
+    elif partes[0] == "reverse":
+        arr.reverse()
+
+
+
