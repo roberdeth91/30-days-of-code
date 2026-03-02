c4f98907d7426cff0ef185e38fac8a6f0ec3e958
roberdeth91 <amavizca.roberto@gmail.com>
2026-03-01 19:50:00 -0700

    auto: backup 2026-03-01 19:50:00

--- python/map.py
+++ python/map.py
@@ -5,18 +5,15 @@ secMaxi = 0
 c=0
 
 for i in arr:
-    if i > maxi and c != 0 :
-        secMaxi = maxi
-        maxi = i
-        print("entro 1;",secMaxi,  i)
-    elif i > maxi :
+    if i > maxi : 
         maxi = i
         c += 1
-        print("entro 2:",maxi, i )
-    elif i > secMaxi:
+    elif maxi > i and c != 0 :
+        secMaxi = maxi
+        maxi = i
+    elif i > secMaxi :
         secMaxi = i
 
-print(maxi)
 print(secMaxi)
 
 
