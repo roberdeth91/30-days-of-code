cdc86e8ba3aecd1881ed7f76ea0acbd4674f7cf0
roberdeth91 <amavizca.roberto@gmail.com>
2026-03-01 20:10:00 -0700

    auto: backup 2026-03-01 20:10:00

--- python/map.py
+++ python/map.py
@@ -5,18 +5,17 @@ secMaxi = 0
 c=0
 
 for i in arr:
-    if i > maxi : 
-        maxi = i
-        c += 1
-        print("loop 1:",maxi, secMaxi)
-    elif maxi > i and c != 0 :
+    if maxi > i and c != 0 :
         secMaxi = maxi
         maxi = i
         print("loop2",maxi, secMaxi)
     elif i < maxi and i > secMaxi :
         secMaxi = i
         print("loop3",maxi, secMaxi)
-
+    elif i > maxi :
+        maxi = input
+        c += 1
+        print("loop 0:",maxi, secMaxi)
 
 print(secMaxi)
 
