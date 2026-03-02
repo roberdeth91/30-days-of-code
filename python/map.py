fe456647a834e84356b26670fc7eb3eadd7acb46
roberdeth91 <amavizca.roberto@gmail.com>
2026-03-01 18:50:00 -0700

    auto: backup 2026-03-01 18:50:00

--- python/map.py
+++ python/map.py
@@ -1,4 +1,14 @@
 n = int(input())
 arr = map(int, input().split())
-
+maxi = 0
+secMaxi = 0
+val = 0
 print (arr)
+
+for i in arr:
+    val = arr[i]
+    if val > maxi:
+        maxi = val
+
+print(maxi)
+
