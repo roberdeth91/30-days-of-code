912f59f16c5be270e687c85f48098ee596e6305b
roberdeth91 <amavizca.roberto@gmail.com>
2026-03-02 23:20:00 -0700

    auto: backup 2026-03-02 23:20:00

--- python/nestedLists.py
+++ python/nestedLists.py
@@ -7,7 +7,7 @@ for i in range(int(input())):
     students.append ([name , score])
     
 students.sort(key=lambda x: x[1], reverse=False)
-print(students[1][0])
+#print(students[1][0])
 
 second_lowest = students[1][1]
 
