0b158a2ffdabc45fa96bdd707e4510cddac4cbaa
roberdeth91 <amavizca.roberto@gmail.com>
2026-03-03 19:05:00 -0700

    auto: backup 2026-03-03 19:05:00

---
+++ python/percentage.py
@@ -0,0 +1,12 @@
+n = int(input())
+student_marks = {}
+
+for _ in range(n):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+query_name = input()
+
+for name, scores in student_marks :
+    if query_name == name :
+        print(scores)
